bbb08d48db5066d2c217de3f7d0ca965f304a8ad
unknown <rud.hardianto@gmail.com>
2022-02-22 11:57:06 +0700

    commit

---
+++ resources/js/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home(props) {
+   const username = props.username;
+   return (
+      <div>
+         <h1>Hallo, nama saya {username}</h1>
+         My Pages <br />
+      </div>
+   );
+}
